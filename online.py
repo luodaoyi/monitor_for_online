2d4df8d9ae0b8dc68cdfd709d9f3d1aa66d299f9
luodaoyi <luodaoyi@gmail.com>
2017-12-22 20:04:37 +0800

    更新online脚本 使用表格推送详细内容

--- online.py
+++ online.py
@@ -5,11 +5,13 @@ import requests
 from bs4 import BeautifulSoup
 
 ## 需要监控的价格
-prices = ['6.99','16.99', '19.99','15.99'] #15.99不是特价款 
-# 通知的server酱的key  https://sc.ftqq.com 这里注册 然后去用微信扫描生成一个key就行了
+prices = ['6.99','16.99','19.99']
+# 通知的server酱的key  https://sc.ftqq.com 这里注册
 keys = [
-    'xxxxxxx',
+    'xxxxxx',
 ]
+# 批量订阅的发送key http://pushbear.ftqq.com/ 注册
+send_key = 'xxxxxx'
 # 检查间隔
 check_interval = 60
 # 服务器列表订购列表
@@ -25,29 +27,62 @@ def get_page():
         return resp.content
 # 发送消息通知
 def send_message(key, text, desp=''):
+
+
     url = f"https://sc.ftqq.com/{key}.send"
     data = {
         'text': text,
         'desp': desp
     }
     requests.post(url, data=data, timeout=timeout)
+
+# 发送消息通知
+def send_message_all(key, text, desp=''):
+    url = 'https://pushbear.ftqq.com/sub'
+    data = {
+        'sendkey':key,
+        'text': text,
+        'desp': desp
+    }
+    requests.post(url, data=data, timeout=timeout)
+
 # 检查每条数据是不是有货
 def check_server(tr):
     tds = tr.find_all('td')
     if tds and tds[-1].find('form'): #有form就是有货啊
         tds_text = [td.text for td in tds]
+        
+        name = tds_text[-8].strip()
+        cpu = tds_text[-7].strip()
+        mem_count = tds_text[-6].replace('Go','').strip() + "GB"
+        disk = "SSD硬盘" if tds_text[-5].strip().find("SSD") else "普通硬盘"
+        disk_count = tds_text[-5].strip()
         price = tds_text[-2].replace(' € pre-tax', '').strip()
-        details = '\n\n'.join(tds_text[:-1]) 
+        count = tds_text[-3].strip()
+
         if price in prices:
             if notified.get(price, 0) < notice_times:
                 send_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                text = f"{price}O有货啦"
-                desp = details + '\n\n 监控时间:   ' + send_time + '\n\n 购买地址:   ' + url
+                text = f"{price}O_{disk}_库存{count}"
+                details = f'配置项|数据 \n'
+                details += f'----|---- \n'
+                details += f'名称|{name} \n'
+                details += f'CPU|{cpu} \n'
+                details += f'内存|{mem_count} \n'
+                details += f'硬盘|{disk_count} \n'
+                details += f'价格|{price} € pre-tax \n'
+                details += f'库存|{count} \n\n'
+                
+                desp ='## 配置信息: \n\n ' + details + '\n\n ##监控时间\n\n' + send_time + '\n\n ##购买地址\n\n ' + url + "\n\n"
                 notified[price] = notified.get(price, 0) + 1
+                #print(desp)
                 print(f"{price}O有货了，第{notified[price]}次通知")
-                for key in keys:
-                    send_message(key, text, desp)
-                    time.sleep(1)
+
+                # 发送单个人的
+                for it_key in keys:
+                    send_message(it_key, text, desp)
+                # 发送push熊的
+                send_message_all(send_key, text, desp)
             else:
                 print(f'{price}O的相关信息超过最大通知次数，不再微信通知...')
         else:
